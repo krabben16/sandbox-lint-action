450d500b128750c7afb15076e75e1ee5930f848e
Lint Action <lint-action@samuelmeuli.com>
2022-06-21 15:19:14 +0000

    Fix code style issues with Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 // https://prettier.io/docs/en/configuration.html
 module.exports = {
-	trailingComma: "es5",
-	tabWidth: 4,
-	semi: false,
-	singleQuote: true,
-};
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
